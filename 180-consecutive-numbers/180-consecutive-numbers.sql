bcb994bda4064c985e90d9eccd5b28d91079d776
rama7993 <61896721+rama7993@users.noreply.github.com>
2022-04-17 16:58:55 +0530

    Time: 796 ms (10.85%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-select distinct(l.num) as ConsecutiveNums #distinct to filter repeating more than 3 consecutives
+
+SELECT DISTINCT(l1.Num) AS ConsecutiveNums
+FROM Logs l1,Logs l2,Logs l3
+WHERE l1.Id = l2.Id - 1 AND l2.Id = l3.Id - 1 AND l1.Num = l2.Num AND l2.Num = l3.Num ;
+
+/*select distinct(l.num) as ConsecutiveNums #distinct to filter repeating more than 3 consecutives
 from Logs l
 where ( (l.id+1,l.num) in (select id,num  from Logs) and (l.id+2,l.num) in (select id,num  from Logs) );
+*/
