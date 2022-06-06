904182a6d77492aae4805be0df24c0084ea84aca
rama7993 <61896721+rama7993@users.noreply.github.com>
2022-06-06 10:08:29 +0530

    Time: 9 ms (48.35%), Space: 6.7 MB (35.47%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    set<vector<string>>ans;
+    bool safe(vector<string>&grid,int i,int j,int n){
+        int row=i,col=j;
+        //left top 45 degree
+        while(i>=0 and j>=0){
+            if(grid[i][j]=='Q')
+                return false;
+            i--;
+            j--;
+        }
+        
+        i=row;
+        j=col;
+        //left 0 degree
+        while( j>=0){
+            if(grid[i][j]=='Q')
+                return false;
+            j--;
+        }
+        
+        i=row;
+        j=col;
+        //left bottom 45 degree
+        while(i<n and j>=0){
+            if(grid[i][j]=='Q')
+                return false;
+            i++;
+            j--;
+        }
+        
+      return true;
+    }
+    void solve(vector<string>&grid,int col,int n){
+        if(col==n){
+            ans.insert(grid);
+        }
+        for(int row=0;row<n;row++){
+            if(safe(grid,row,col,n)){
+                grid[row][col]='Q';
+                solve(grid,col+1,n);
+                grid[row][col]='.';//backtrack
+            }
+          
+        }
+    }
+     
+public:
+    int totalNQueens(int n) {
+        vector<string> grid(n,string(n,'.'));
+        solve(grid,0,n);
+        return ans.size();
+    }
+};
