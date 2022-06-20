76511e5d94eff7d533a4ac2054e86c80b007bb34
rama7993 <61896721+rama7993@users.noreply.github.com>
2022-06-20 11:38:13 +0530

    Time: 94 ms (61.88%), Space: 74 MB (22.77%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -2,15 +2,8 @@ class TrieNode {
     public:
     TrieNode* child[26];
     bool is_end;
-};
-
-class Solution {
-private:
-    static bool comp(const string &a,const string &b){
-        return a.size()>b.size();
-    }
-    TrieNode *root=new TrieNode();
-    int insert(string &word){
+    
+    int insert(string &word,TrieNode *root){
         TrieNode *temp=root;
         bool flag=false;//check for new branch or existing
         for(char &ch:word){
@@ -24,13 +17,22 @@ private:
         temp->is_end=true;
         return flag?word.size():0;
     }
+};
+
+class Solution {
+private:
+    static bool comp(const string &a,const string &b){
+        return a.size()>b.size();
+    }
+    
 public:
     int minimumLengthEncoding(vector<string>& words) {
+        TrieNode *root=new TrieNode();
         sort(words.begin(),words.end(),comp);
         int cnt=0;
         for(auto &word:words){
             reverse(word.begin(),word.end());//to match 
-            int ans=insert(word);
+            int ans=root->insert(word,root);
             if(ans)
                 cnt+=ans+1;
         }
