9ed18819fb7dc9e4c5bff7972e6af0ae386b323a
YERRAM RAMA KOTI REDDY <61896721+rama7993@users.noreply.github.com>
2022-09-13 10:25:03 +0530

    Time: 140 ms (54.55%), Space: 45.1 MB (100.00%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.ts
@@ -0,0 +1,25 @@
+function validUtf8(data: number[]): boolean {
+ let rembits:number=0
+ for(let num of data){
+     if(rembits!=0){
+         if(num>>6 == 0b10) 
+             rembits-=1 //n-1 bits
+         else 
+             return false
+     }
+     else{
+            if(num>>7 == 0b0) 
+                rembits=0 //1 byte
+            else if (num>>5 == 0b110)
+                rembits=1 //2 bytes
+            else if (num>>4 == 0b1110) 
+                rembits=2 //3 bytes
+            else if (num>>3 == 0b11110)
+                rembits=3 //4 bytes
+            else
+                return false
+     }
+ }
+    return rembits == 0
+    
+};
