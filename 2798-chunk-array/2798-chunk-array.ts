a1043a3159ad182907c1dbb69bc890904a16d905
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-01-06 10:28:02 +0530

    Time: 56 ms (77.87%), Space: 52.1 MB (69.54%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.ts
+++ 2798-chunk-array/2798-chunk-array.ts
@@ -6,7 +6,7 @@
 var chunk = function (arr, size) {
     let res = [], n = arr.length;
     let start = 0, end = size;
-    for (let i = 0; i < n; i++) {
+    while (end < n + size) {
         const chunk = end < n ? arr.slice(start, end) : arr.slice(start);
         chunk.length && res.push(chunk);
         start = end;
