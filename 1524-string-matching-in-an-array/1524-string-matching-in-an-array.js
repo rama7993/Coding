48d03f9c30e5ace5093c28424091d32a4342ad9b
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-01-07 09:22:27 +0530

    Time: 1 ms (94.33%), Space: 49.1 MB (72.68%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function (words) {
+    const n = words.length;
+    const ans = [];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j) continue;
+            if (words[j].indexOf(words[i]) !== -1) {
+                ans.push(words[i]);
+                break;
+            }
+        }
+    }
+
+    return ans;
+};
