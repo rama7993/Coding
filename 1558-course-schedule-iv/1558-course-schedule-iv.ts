3135879700f74410f0e1880cebd359eedce722d9
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-01-28 10:49:12 +0530

    Time: 39 ms (89.74%), Space: 60.3 MB (76.92%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.ts
+++ 1558-course-schedule-iv/1558-course-schedule-iv.ts
@@ -1,28 +1,28 @@
 function checkIfPrerequisite(numCourses: number, prerequisites: number[][], queries: number[][]): boolean[] {
-    const adjList: number[][] = Array.from({ length: numCourses }, () => []);
+    const adjList = Array.from({ length: numCourses }, () => []);
+    const inDegree = Array(numCourses).fill(0);
     for (const [u, v] of prerequisites) {
         adjList[u].push(v);
+        inDegree[v]++;
     }
 
-    // Function to perform DFS and mark prerequisites
-    const isPrerequisite = (start: number) => {
-        const visited = new Set<number>();
-        const dfs = (course: number) => {
-            if (visited.has(course)) return;
-            visited.add(course);
-            for (const next of adjList[course]) {
-                dfs(next);
-            }
-        };
-        dfs(start);
-        visited.delete(start); // Remove the start course itself from the set
-        return visited;
-    };
-
-    const prerequisitesMap = new Map<number, Set<number>>();
+    //Topological sorting 
+    const q = []//queue;
+    const preqArr = Array.from({ length: numCourses }, () => new Set<number>())
     for (let i = 0; i < numCourses; i++) {
-        prerequisitesMap.set(i, isPrerequisite(i));
+        inDegree[i] === 0 && q.push(i);
+    }
+    while (q.length > 0) {
+        const node = q.shift();
+        for (const u of adjList[node]) {
+            preqArr[u].add(node); // add as prerequisite for all adj nodes;
+            for (const preq of preqArr[node]) { // add prequisites of current node as well to adj nodes 
+                preqArr[u].add(preq);
+            }
+            inDegree[u]--;
+            inDegree[u] === 0 && q.push(u);
+        }
     }
 
-    return queries.map(([u, v]) => prerequisitesMap.get(u).has(v));
-}
+    return queries.map(([u, v]) => preqArr[v].has(u));
+};
