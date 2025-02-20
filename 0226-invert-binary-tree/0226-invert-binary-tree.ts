c98cccfe0925546cdefccc1ca39d2f012105d888
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-02-20 21:55:04 +0530

    Time: 0 ms (100%), Space: 57.1 MB (12.15%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.ts
+++ 0226-invert-binary-tree/0226-invert-binary-tree.ts
@@ -14,9 +14,8 @@
 
 function invertTree(root: TreeNode | null): TreeNode | null {
     if (!root) return null;
-    root.left = invertTree(root.left);
-    root.right = invertTree(root.right);
     [root.left, root.right] = [root.right, root.left];
-
+    invertTree(root.left);
+    invertTree(root.right);
     return root;
 };
