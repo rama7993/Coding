e388079332f6c5ee94c79f83e7ff0912f94ac9c2
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-02-20 11:09:30 +0530

    Time: 7 ms (20.89%), Space: 42 MB (29.37%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        String ans = "";
+        for (int i = 0; i < nums.length; i++) {
+            ans += (nums[i].charAt(i) == '0') ? '1' : '0';
+        }
+        return ans;
+    }
+}
