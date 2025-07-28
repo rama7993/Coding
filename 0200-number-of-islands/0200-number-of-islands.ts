2c092a8bc4fee8684cc35ba2c7e59b65ea805df5
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-07-28 11:27:50 +0530

    Time: 81 ms (28.9%), Space: 64.8 MB (41.15%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -0,0 +1,32 @@
+function numIslands(grid: string[][]): number {
+    const m = grid.length, n = grid[0].length;
+    const visited: boolean[][] = Array.from({ length: m }, () => Array(n).fill(false));
+    let islands = 0;
+    const dir = [[-1, 0], [0, 1], [0, -1], [1, 0]];
+    const queue = [];
+
+    const bfs = (i, j) => {
+        while (queue.length) {
+            const [x, y] = queue.shift();
+            visited[x][y] = true;
+            for (let [dx, dy] of dir) {
+                const nx = x + dx, ny = y + dy;
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && grid[nx][ny] === '1') {
+                    queue.push([nx, ny]);
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1' && !visited[i][j]) {
+                islands++;
+                queue.push([i, j]);
+                bfs(i, j);
+            }
+        }
+    }
+    return islands;
+};
