3afddc39420845282f5a14153f2b90caf96a1238
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2025-07-28 11:30:24 +0530

    Time: 59 ms (78.61%), Space: 64.5 MB (42.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -3,27 +3,32 @@ function numIslands(grid: string[][]): number {
     const visited: boolean[][] = Array.from({ length: m }, () => Array(n).fill(false));
     let islands = 0;
     const dir = [[-1, 0], [0, 1], [0, -1], [1, 0]];
-    const queue = [];
 
-    const bfs = (i, j) => {
+    const bfs = (i: number, j: number) => {
+        const queue: [number, number][] = [[i, j]];
+        visited[i][j] = true;
+
         while (queue.length) {
-            const [x, y] = queue.shift();
-            visited[x][y] = true;
+            const [x, y] = queue.shift()!;
             for (let [dx, dy] of dir) {
                 const nx = x + dx, ny = y + dy;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && grid[nx][ny] === '1') {
+                if (
+                    nx >= 0 && nx < m &&
+                    ny >= 0 && ny < n &&
+                    !visited[nx][ny] &&
+                    grid[nx][ny] === '1'
+                ) {
                     queue.push([nx, ny]);
                     visited[nx][ny] = true;
                 }
             }
         }
-    }
+    };
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] === '1' && !visited[i][j]) {
                 islands++;
-                queue.push([i, j]);
                 bfs(i, j);
             }
         }
