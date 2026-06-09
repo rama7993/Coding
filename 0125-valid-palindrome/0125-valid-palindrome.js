58ffef8c50dd1d7f8a220025adcddca95bf46d96
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-09 19:51:01 +0530

    Time: 21 ms (5.29%), Space: 60.8 MB (23.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,21 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    const strg = s.replace(/[^A-Za-z0-9]+/g, '').toLowerCase();
-    return strg == strg.split('').reverse().join('');
+    //const isAlnum = (ch) => /[a-z0-9]/i.test(ch);
+    const isAlnum = (ch) => {
+        const reg = new RegExp('[a-z0-9]', 'i');
+        return reg.test(ch);
+    }
+
+    let i = 0, j = s.length - 1;
+    while (i < j) {
+        if (!isAlnum(s[i])) i++;
+        else if (!isAlnum(s[j])) j--;
+        else if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+        else {
+            i++; j--;
+        }
+
+    }
+    return true;
 };
