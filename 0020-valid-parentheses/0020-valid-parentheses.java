83cf5735fbb1fb8e1dc7592883a510ac51595e91
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-11 01:52:11 +0530

    Time: 2 ms (98.01%), Space: 42 MB (99.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(')
+                st.push(')');
+            else if(ch=='[')
+                st.push(']');
+            else if(ch=='{')
+                st.push('}');
+            else if(st.isEmpty() || st.peek()!=ch){
+                return false;
+            }
+            else
+                st.pop();
+        }
+        return st.isEmpty();
+    }
+}
