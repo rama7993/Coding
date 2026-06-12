2e8823635a61509932603ebe8597a96cb731a275
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-12 22:58:00 +0530

    Time: 13 ms (27.09%), Space: 60.8 MB (14.69%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -4,39 +4,37 @@
  */
 var orangesRotting = function (grid) {
     const m = grid.length, n = grid[0].length;
-    let fresh = 0, minutes = 0;
-    const q = [];
-    const dir = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const q = new Queue();
+    let time = 0;
+    let fresh = 0;
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 2) q.push([i, j]);
-            else if (grid[i][j] === 1) fresh++;
+            if (grid[i][j] === 1) fresh++;
+            else if (grid[i][j] === 2) q.enqueue([i, j]);
         }
     }
 
     if (fresh === 0) return 0;
 
-    while (q.length && fresh > 0) {
-        let size = q.length;
+    while (!q.isEmpty() && fresh > 0) {
+        const size = q.size();
 
         for (let i = 0; i < size; i++) {
-            const [x, y] = q.shift();
-
-            for (let [dx, dy] of dir) {
-                const nx = x + dx;
-                const ny = y + dy;
-
+            const [x, y] = q.dequeue();
+            for (const [dx, dy] of dirs) {
+                const nx = x + dx, ny = y + dy;
                 if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] === 1) {
+                    q.enqueue([nx, ny]);
                     grid[nx][ny] = 2;
                     fresh--;
-                    q.push([nx, ny]);
                 }
             }
         }
 
-        minutes++;
+        time++;
     }
 
-    return fresh === 0 ? minutes : -1;
+    return fresh === 0 ? time : -1;
 };
