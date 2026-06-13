2826cef8ebd391a0748a8581a20f84c05917ceef
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-13 23:56:03 +0530

    Time: 1 ms (49.09%), Space: 59.1 MB (24.62%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -7,25 +7,28 @@
  */
 var floodFill = function (image, sr, sc, color) {
     const m = image.length, n = image[0].length;
-    const orgColor = image[sr][sc];
     const dir = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    const queue = [[sr, sc]];
-    if (orgColor === color) return image;
+    const original = image[sr][sc]; // Visit only connected cells with this original color.
+    if (original === color) return image;
 
-    while (queue.length > 0) {
-        const [x, y] = queue.shift();
-        if (image[x][y] === orgColor) {
-            image[x][y] = color;
-            for (let [dx, dy] of dir) {
-                const nx = x + dx;
-                const ny = y + dy;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && image[nx][ny] === orgColor) {
-                    queue.push([nx, ny]);
-                }
+    const dfs = (x, y) => {
+        if (x < 0 || x >= m || y < 0 || y >= n || image[x][y] !== original) return;
 
+        image[x][y] = color;//visited
+
+        for (const [dx, dy] of dir) {
+            dfs(x + dx, y + dy);
+        }
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === sr && j === sc) {
+                dfs(i, j);
             }
         }
     }
+
 
     return image;
 };
