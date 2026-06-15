90d714a6ffec423f779a063b061c4e49c72980a2
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-15 22:35:36 +0530

    Time: 15 ms (27.16%), Space: 72.1 MB (28.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,24 +4,16 @@
  */
 var productExceptSelf = function (nums) {
     const n = nums.length;
-    const prefix = Array(n);
-    const suffix = Array(n);
-    const res = Array(n);
+    const res = Array(n).fill(1);
 
-    prefix[0] = nums[0];
     for (let i = 1; i < n; i++) {
-        prefix[i] = nums[i] * prefix[i - 1];
+        res[i] = nums[i - 1] * res[i - 1];
     }
-
-    suffix[n - 1] = nums[n - 1];
-    for (let i = n - 2; i >= 0; i--) {
-        suffix[i] = nums[i] * suffix[i + 1];
-    }
-
-    for (let i = 0; i < n; i++) {
-        res[i] =
-            (i > 0 ? prefix[i - 1] : 1) *
-            (i < n - 1 ? suffix[i + 1] : 1);
+    console.log(res);
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] *= suffix;
+        suffix *= nums[i];
     }
 
     return res;
