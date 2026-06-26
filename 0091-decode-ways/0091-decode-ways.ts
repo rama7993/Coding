e865f3ae89bdfc2723674da5ed34c1808a5cf334
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-26 19:55:16 +0530

    Time: 0 ms (100%), Space: 55.3 MB (85.07%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.ts
+++ 0091-decode-ways/0091-decode-ways.ts
@@ -1,25 +1,24 @@
 function numDecodings(s: string): number {
     const n = s.length;
-    let prev2 = 1; //empty 
-    let prev1 = s.charAt(0) === '0' ? 0 : 1;
+    const dp = Array(n + 1).fill(0); // decode first i
+    dp[0] = 1; //empty 
+    dp[1] = s.charAt(0) === '0' ? 0 : 1;
 
     for (let i = 2; i <= n; i++) {
-        let curr = 0;
+        let ways = 0;
 
         //1 digit
         if (s.charAt(i - 1) !== '0') {
-            curr += prev1;
+            ways += dp[i - 1];
         }
 
         //2 digit
         if (
             s[i - 2] === '1' || (s[i - 2] === '2' && s[i - 1] <= '6')
         ) {
-            curr += prev2;
+            ways += dp[i - 2];
         }
-
-        prev2 = prev1;
-        prev1 = curr;
+        dp[i] = ways;
     }
-    return prev1;
+    return dp[n];
 };
