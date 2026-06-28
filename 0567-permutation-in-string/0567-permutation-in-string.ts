c2033b06c6b9ef63dda8c8c19b47540940cc2395
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-06-28 18:24:59 +0530

    Time: 60 ms (21.53%), Space: 64.3 MB (10.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.ts
+++ 0567-permutation-in-string/0567-permutation-in-string.ts
@@ -1,37 +1,24 @@
 function checkInclusion(s1: string, s2: string): boolean {
-    const mp1 = new Map<string, number>();
-    const mp2 = new Map<string, number>();
-
-    for (let char of s1) {
-        const val = mp1.get(char) || 0;
-        mp1.set(char, val + 1);
-    }
-
     const m = s1.length, n = s2.length;
+    if (m > n) return false;
+    const freq1 = Array(26).fill(0);
+    const freq2 = Array(26).fill(0);
+
+    for (const ch of s1) {
+        let idx = ch.charCodeAt(0) - 'a'.charCodeAt(0);
+        freq1[idx]++;
+    }
 
     for (let i = 0; i < n; i++) {
-        const ch = s2[i];
-        mp2.set(ch, (mp2.get(ch) || 0) + 1);
+        let idx = s2[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        freq2[idx]++;
 
-        if (i >= m) {
-            const outChar = s2[i - m];
-            mp2.set(outChar, mp2.get(outChar) - 1);
-            if (mp2.get(outChar) === 0) {
-                mp2.delete(outChar);
-            }
+        if (i >= m) { //shrink window
+            let idx = s2[i - m].charCodeAt(0) - 'a'.charCodeAt(0);
+            freq2[idx]--;
         }
 
-        if (isEqualValues(mp1, mp2)) return true;
+        if (JSON.stringify(freq1) === JSON.stringify(freq2)) return true;
     }
-
     return false;
-}
-
-
-function isEqualValues(mp1: Map<string, number>, mp2: Map<string, number>): boolean {
-    if (mp1.size !== mp2.size) return false;
-    for (let [key, val] of mp1) {
-        if (mp2.get(key) !== val) return false;
-    }
-    return true;
-}
+};
