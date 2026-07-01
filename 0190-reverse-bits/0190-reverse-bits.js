c9e096dd0b8a9859f548d65f355e16baf0f6d41c
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-07-02 00:20:52 +0530

    Time: 58 ms (9.81%), Space: 53.8 MB (75.47%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function (n) {
+    let res = 0;
+
+    for (let i = 0; i < 32; i++) {
+        res <<= 1;
+        res |= n & 1;
+        n >>>= 1;
+    }
+
+    return res >>> 0;
+};
