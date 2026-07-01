1e1d4f84f319349cf9a184b08bb209c2cacb7795
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-07-01 19:03:28 +0530

    Time: 203 ms (5.16%), Space: 62.9 MB (22.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -1,21 +1,38 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
 function setZeroes(matrix: number[][]): void {
+    const dir = [[0, 1], [0, -1], [1, 0], [-1, 0]];
     const m = matrix.length, n = matrix[0].length;
-    const set = new Set<string>();
+    const visited = Array.from({ length: m }, () => Array(n).fill(false));
+
+    const dfs = (r: number, c: number, dr: number, dc: number) => {
+        if (r < 0 || r >= m || c < 0 || c >= n) return;
+        if (visited[r][c] && matrix[r][c] !== 0) return;
+
+        if (matrix[r][c] !== 0) {
+            visited[r][c] = true;
+        }
+
+        matrix[r][c] = 0;
+
+        // Keep moving in the SAME direction
+        dfs(r + dr, c + dc, dr, dc);
+    };
+
+
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (matrix[i][j] === 0) {
-                set.add('r' + i); // mark row i
-                set.add('c' + j); // mark column j
-            }
-        }
-    }
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (set.has('r' + i) || set.has('c' + j)) {
-                matrix[i][j] = 0;
+            if (matrix[i][j] === 0 && !visited[i][j]) {
+
+                visited[i][j] = true;
+
+                for (const [dr, dc] of dir) {
+                    dfs(i + dr, j + dc, dr, dc);
+                }
             }
         }
     }
-}
+};
