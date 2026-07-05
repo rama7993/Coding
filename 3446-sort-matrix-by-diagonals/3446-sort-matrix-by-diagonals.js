d5df592d21f9f672ccba9d37c7f2c6d3cbd435d0
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-07-05 18:24:34 +0530

    Time: 9 ms (75%), Space: 65.1 MB (50%) - LeetHub

---
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var sortMatrix = function (grid) {
+    const n = grid.length;
+    const mp = new Map();
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mp.has(j - i)) {
+                mp.get(j - i).push(grid[i][j]);
+            } else {
+                mp.set(j - i, [grid[i][j]]);
+            }
+        }
+    }
+
+
+    for (const [key, val] of mp) {
+        val.sort((a, b) => key <= 0 ? b - a : a - b);
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            grid[i][j] = mp.get(j - i).shift();
+        }
+    }
+
+    return grid;
+};
