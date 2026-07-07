7c899d955a5b9b811ccb6f6c949b8f88a9994228
Yerram Rama Koti Reddy <61896721+rama7993@users.noreply.github.com>
2026-07-07 11:49:15 +0530

    Time: 0 ms (100%), Space: 55 MB (65.43%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.ts
+++ 0500-keyboard-row/0500-keyboard-row.ts
@@ -1,26 +1,9 @@
 function findWords(words: string[]): string[] {
-    const mp = new Map();
+    const rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
 
-    let row1 = new Set("qwertyuiop"), row2 = new Set("asdfghjkl"), row3 = new Set("zxcvbnm");
-
-    let res = [];
-
-    for (const word of words) {
+    return words.filter((word) => {
         const lower = word.toLowerCase();
-
-        let row;
-        if (row1.has(lower[0])) row = row1;
-        else if (row2.has(lower[0])) row = row2;
-        else row = row3;
-
-        let valid = true;
-        for (let ch of lower) {
-            if (!row.has(ch)) {
-                valid = false; break;
-            }
-        }
-        valid && res.push(word);
-    }
-
-    return res;
+        const idx = rows.findIndex(r => r.includes(lower[0]));
+        return [...lower].every(ch => rows[idx].includes(ch));
+    })
 };
